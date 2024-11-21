52904ebf21b0722b1953d4c773a3b9756d54aa96
Gusarov Anatoly <gusarov.tolia@mail.ru>
2024-11-21 21:20:51 +0500

    feat: add open/close form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,19 +2,22 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [openForm, setOpenForm] = useState(true);
+	const handleClick = () => setOpenForm((prevState) => !prevState);
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton OnClick={handleClick} openForm={openForm} />
 			<aside
-				className={styles.container}>
+				className={clsx(styles.container, {
+					[styles['container_open']]: openForm,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
-						<Button
-							title='Сбросить'
-							type='reset'
-						/>
+						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
