4743a733f4be6b0b030689efdd30e6f77e5995e4
Gusarov Anatoly <gusarov.tolia@mail.ru>
2024-11-22 11:31:59 +0500

    feat: add handleChangeParams

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,36 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
 import clsx from 'clsx';
-
+import { Select } from '../select';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { Text } from '../text';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Spacing } from '../spacing';
 export const ArticleParamsForm = () => {
 	const [openForm, setOpenForm] = useState(true);
+	const [params, setParams] = useState(defaultArticleState);
 	const handleClick = () => setOpenForm((prevState) => !prevState);
+	const handleChangeParams = (
+		value: string,
+		field: keyof ArticleStateType,
+		options: OptionType[]
+	) => {
+		const needObj = options.find((obj) => obj.value === value);
+		setParams({ ...params, [field]: needObj });
+	};
+
 	return (
 		<>
 			<ArrowButton OnClick={handleClick} openForm={openForm} />
@@ -16,6 +40,57 @@ export const ArticleParamsForm = () => {
 					[styles['container_open']]: openForm,
 				})}>
 				<form className={styles.form}>
+					<Text weight={800} size={31} align='left' uppercase>
+						Задайте параметры
+					</Text>
+					<Spacing size={50} />
+					<Select
+						title='Шрифт'
+						selected={params.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(e) =>
+							handleChangeParams(e.value, 'fontFamilyOption', fontFamilyOptions)
+						}
+					/>
+					<Spacing size={50} />
+					<RadioGroup
+						name='Размер Шрифта'
+						options={fontSizeOptions}
+						selected={params.fontSizeOption}
+						title='Размер Шрифта'
+						onChange={(e) =>
+							handleChangeParams(e.value, 'fontSizeOption', fontSizeOptions)
+						}
+					/>
+					<Spacing size={50} />
+					<Select
+						title='Цвет Шрифта'
+						selected={params.fontColor}
+						options={fontColors}
+						onChange={(e) =>
+							handleChangeParams(e.value, 'fontColor', fontColors)
+						}
+					/>
+					<Spacing size={50} />
+					<Separator />
+					<Spacing size={50} />
+					<Select
+						title='Цвет Фона'
+						selected={params.backgroundColor}
+						options={backgroundColors}
+						onChange={(e) =>
+							handleChangeParams(e.value, 'backgroundColor', backgroundColors)
+						}
+					/>
+					<Spacing size={50} />
+					<Select
+						title='Ширина Контента'
+						selected={params.contentWidth}
+						options={contentWidthArr}
+						onChange={(e) =>
+							handleChangeParams(e.value, 'contentWidth', contentWidthArr)
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
