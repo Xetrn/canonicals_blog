2fc541d21226770fa4f71db0fb06968af77ac79f
Egor <OneTells@ya.ru>
2024-12-11 20:15:39 +0500

    New Version

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react';
-import clsx from 'clsx';
-import arrow from 'src/images/arrow.svg';
+import arrow from '../../images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export type ArrowButtonProps = {
+	isOpened: boolean;
+	onClick?: OnClick;
+};
+
+export const ArrowButton = ({ isOpened, onClick }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}
-		>
+			onClick={onClick}
+			className={`${styles.container} ${
+				isOpened ? styles.container_open : ''
+			}`}>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={styles.arrow}
+				className={`${styles.arrow} ${isOpened ? styles.arrow_open : ''}`}
 			/>
 		</div>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,113 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
+import { ArrowButton } from '../arrow-button';
+import { Button } from '../button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Dispatch, FormEvent, SetStateAction, useState } from 'react';
 
-export const ArticleParamsForm = () => {
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+
+import { Spacing } from '../spacing';
+import { Text } from '../text';
+import { Select } from '../select';
+import { Separator } from '../separator';
+import { RadioGroup } from '../radio-group';
+
+export type ArticleFormProps = {
+	isFormOpened: boolean;
+	setIsFormOpened: Dispatch<SetStateAction<boolean>>;
+	setArticleState: Dispatch<SetStateAction<ArticleStateType>>;
+};
+
+export const ArticleParamsForm = ({
+	isFormOpened,
+	setIsFormOpened,
+	setArticleState,
+}: ArticleFormProps) => {
+	const [currentState, setCurrentState] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton
+				isOpened={isFormOpened}
+				onClick={() => setIsFormOpened(!isFormOpened)}
+			/>
 			<aside
-				className={styles.container}>
-				<form className={styles.form}>
+				className={`${styles.container} ${
+					isFormOpened ? styles.container_open : ''
+				}`}>
+				<form
+					className={styles.form}
+					onSubmit={(event: FormEvent) => {
+						event.preventDefault();
+						setArticleState(currentState);
+					}}
+					onReset={() => {
+						setCurrentState(defaultArticleState);
+						setArticleState(defaultArticleState);
+					}}>
+					<Text family='open-sans' weight={800} size={31} uppercase>
+						Задайте параметры
+					</Text>
+					<Spacing size={50} />
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={currentState.fontFamilyOption}
+						onChange={(selected: OptionType) =>
+							setCurrentState({ ...currentState, fontFamilyOption: selected })
+						}
+					/>
+					<Spacing size={50} />
+					<RadioGroup
+						name='size'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={currentState.fontSizeOption}
+						onChange={(selected: OptionType) =>
+							setCurrentState({ ...currentState, fontSizeOption: selected })
+						}
+					/>
+					<Spacing size={50} />
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={currentState.fontColor}
+						onChange={(selected: OptionType) =>
+							setCurrentState({ ...currentState, fontColor: selected })
+						}
+					/>
+					<Spacing size={50} />
+					<Separator />
+					<Spacing size={50} />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={currentState.backgroundColor}
+						onChange={(selected: OptionType) =>
+							setCurrentState({ ...currentState, backgroundColor: selected })
+						}
+					/>
+					<Spacing size={50} />
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={currentState.contentWidth}
+						onChange={(selected: OptionType) =>
+							setCurrentState({ ...currentState, contentWidth: selected })
+						}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button
-							title='Сбросить'
-							type='reset'
-						/>
+						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
