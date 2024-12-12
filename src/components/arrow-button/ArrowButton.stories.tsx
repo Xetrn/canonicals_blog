f4f1c7ded035097a157b0cec8685f0a643cc60f3
q4w3e2r1 <artem.zarafutdinov@urfu.me>
2024-12-12 17:52:33 +0500

    done

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -1,20 +1,36 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { ArrowButton, ArrowButtonProps } from './ArrowButton';
 
-import { ArrowButton } from './ArrowButton';
+export default {
+  title: 'Components/ArrowButton',
+  component: ArrowButton,
+  argTypes: {
+    onClick: { action: 'clicked' },
+    isOpen: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Состояние открытия формы',
+      defaultValue: false,
+    },
+  },
+  args: {
+    onClick: () => {},
+    isOpen: false,
+  },
+} as Meta<typeof ArrowButton>;
 
-const meta: Meta<typeof ArrowButton> = {
-	component: ArrowButton,
+const Template: Story<ArrowButtonProps> = (args) => <ArrowButton {...args} />;
+
+export const Closed = Template.bind({});
+Closed.args = {
+  isOpen: false,
 };
+Closed.storyName = 'Закрытое состояние';
 
-export default meta;
-type Story = StoryObj<typeof ArrowButton>;
-
-export const ArrowButtonStory: Story = {
-	render: () => {
-		return (
-			<>
-				<ArrowButton />
-			</>
-		);
-	},
+export const Open = Template.bind({});
+Open.args = {
+  isOpen: true,
 };
+Open.storyName = 'Открытое состояние';
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,18 +2,101 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
+import { defaultArticleState, ArticleStateType, fontFamilyOptions, fontSizeOptions, backgroundColors, fontColors, contentWidthArr } from 'src/constants/articleProps';
+import { Spacing } from '../spacing';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Text } from 'components/text';
+import { Separator } from '../separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onApply: (selectedOption: ArticleStateType) => void;
+	isOpen: boolean;
+	onToggle: () => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const { onApply, isOpen, onToggle } = props;
+
+	const formRef = useRef<HTMLFormElement>(null);
+	const [style, setStyle] = useState<ArticleStateType>(defaultArticleState);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (isOpen && formRef.current && !formRef.current.contains(event.target as Node)) {
+				onToggle();
+			}	
+		};
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, [isOpen]);
+
+	const handleReset = () => {
+		setStyle(defaultArticleState);
+		onApply(defaultArticleState);
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply(style);
+	};
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton onClick={onToggle} isOpen={isOpen} />
 			<aside
-				className={styles.container}>
-				<form className={styles.form}>
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				ref={formRef}
+			>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text as='h2' size={31} weight={800} uppercase>
+                		Задайте параметры
+					</Text>
+					<Spacing size={50} />
+					<Select
+						selected={style.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => setStyle({...style, fontFamilyOption: option})}
+						title='Шрифт'
+					/>
+					<Spacing size={50} />
+					<RadioGroup
+						options={fontSizeOptions}
+						selected={style.fontSizeOption}
+						onChange={(option) => setStyle({...style, fontSizeOption: option})}
+						title='Размер шрифта'
+						name='font-size'
+					/>
+					<Spacing size={50} />
+					<Select
+						selected={style.fontColor}
+						options={fontColors}
+						onChange={(option) => setStyle({...style, fontColor: option})}
+						title='Цвет шрифта'
+					/>
+					<Spacing size={50} />
+					<Separator />
+					<Spacing size={50} />
+					<Select
+						selected={style.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => setStyle({...style, backgroundColor: option})}
+						title='Цвет фона'
+					/>
+					<Spacing size={50} />
+					<Select
+						selected={style.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => setStyle({...style, contentWidth: option})}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button
 							title='Сбросить'
 							type='reset'
+							onClick={handleReset}
 						/>
 						<Button title='Применить' type='submit' />
 					</div>
