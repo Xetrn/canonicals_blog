bb7e0a49f14c09f23364c69366ba5faed5388b7b
Andrew 61X Tiunov <astermiizek@gmail.com>
2024-12-13 00:16:01 +0500

    first commit

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,25 +1,34 @@
-import { useState } from 'react';
+import { FC } from 'react';
 import clsx from 'clsx';
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
+type ArrowButtonProps = {
+	OnClick?: OnClick;
+	isOpened?: boolean;
+};
+
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export const ArrowButton: FC<ArrowButtonProps> = ({ OnClick, isOpened }) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			onClick={OnClick}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}
-		>
+			className={clsx(styles.container, {
+				[styles['container_open']]: isOpened,
+			})}>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={styles.arrow}
+				className={clsx(styles.arrow, {
+					[styles['arrow_open']]: isOpened,
+				})}
 			/>
 		</div>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,19 +2,24 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpened, setIsOpened] = useState(false);
+
+	const handleClick = () => setIsOpened((prevState) => !prevState);
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton OnClick={handleClick} isOpened={isOpened} />
 			<aside
-				className={styles.container}>
+				className={clsx(styles.container, {
+					[styles['container_open']]: isOpened,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
-						<Button
-							title='Сбросить'
-							type='reset'
-						/>
+						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
